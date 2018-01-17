67667b5d13213c9aabf2f5f37957eb6b4283c4ca
Sourav Dutta <souravbpd@gmail.com>
2018-01-17 22:37:29 +0530

    Added Lazy Loading Module Signed-off-by: Sourav Dutta <souravbpd@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FirstComponent } from './first/first.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: FirstComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'lazy',
+    loadChildren: 'app/lazy/lazy.module#LazyModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent
   ],
   imports: [
     BrowserModule,
